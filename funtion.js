b4555fb0a201c9e0a970a01340453503ff624a18
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-07-19 00:48:39 +0530

    addon fn

---
+++ funtion.js
@@ -0,0 +1,44 @@
+// function idea(){
+//     alert("fn working")
+// }
+// idea()
+
+
+
+// no para & no return
+// -------------------------
+// function sum(){
+//     let a=parseInt(prompt("enter 1st no")) 
+// let b=parseInt(prompt("enter 2nd no"))
+//     let c=a+b
+//     // alert(c)
+//     console.log(c)
+// }
+// sum()
+   
+
+// no para & return
+// -------------------------------
+
+// function add(){
+//     let a=10;
+//     let b=20;
+//     return a+b
+// }
+// // console.log(add())
+// let p=add()  
+// console.log(p);
+
+// with para & return 
+// ----------------------------------
+
+
+
+
+
+
+
+
+
+// with para & no return
+// -----------------------------------------
--- object.js
+++ object.js
@@ -3,7 +3,7 @@
 // console.log(obj.name)//here .is access member operator
 
 
-let emp={id:101,name:"kamlesh",age:25,salary:25000,joing:25/6/2024,PF:26000}
+// let emp={id:101,name:"kamlesh",age:25,salary:25000,joing:25/6/2024,PF:26000}
 // console.log(emp)
 // for(let k in emp)
 //     // console.log(k)//key print
@@ -12,11 +12,70 @@ let emp={id:101,name:"kamlesh",age:25,salary:25000,joing:25/6/2024,PF:26000}
 // let k=Object.values(emp)
 // console.log(emp)//key+value
 // console.log(k)//indexs+keys
-// console.log(k)indexs+values
+// console.log(k)//indexs+values
 // console.log(k[0])
-let k=Object.keys(emp)
-console.log(k[0])
+// let k=Object.keys(emp)
+// console.log(k[0])
+
+
+// let emp={
+//     id:1,
+//     name:"kamlesh",
+//     age:23,
+//     dessiganation:"Engineer", 
+//     package:"12lpa" ,
+
+//     arr:[1,2,3],
+
+
+//     fun:function(){
+//         alert("funtion calling")
+//     },
 
 
 
+//     account:{
+//         bank_name:"HDFC",
+//         account_no:1233445,
+//         account_balance:120000
+//     }
+
+// }
+// console.log(emp.dessiganation)
+
+// console.log(emp.arr[2])
+
+// emp.fun()
+
+// console.log(emp.account.account_balance)
+
+
+
+
+// let obj={
+//  id:parseInt(prompt("Enter the id")),
+//  name:prompt("Enter the name"),
+//  age:parseInt(prompt("enter the age")),
+// }
+// console.log(obj)
+
+
+
+// let ob={
+//     name:"kamlesh",
+//     age:45
+// }
+// let ob1={
+//     location:"india",
+// }
+// Object.assign(ob,ob1) //two object merge
+// console.log(ob);
+
+
+// delete ob.age    //any key & value delete
+// console.log(ob);
+
+
+// ob.designation="hr";//any key & value add
+// console.log(ob);
 
