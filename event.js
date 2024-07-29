4d1d06918da5f05885a7a5051817b722bf8345c8
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-07-29 22:24:50 +0530

    addon event

--- event.js
+++ event.js
@@ -4,9 +4,9 @@
 // function fun(){
 //     alert("event dubble cclicked working")
 // }
-function fun1(){
-    alert("event Right cclicked working")
-}
+// function fun1(){
+//     alert("event Right cclicked working")
+// }
 
 
 // function data(){
@@ -14,3 +14,9 @@ function fun1(){
 //     let number2=parseInt(document.getElementById('num2').value) ;
 // document.write(number1+number2);
 // }
+
+function data() {
+    let number1 = document.getElementById('num1').value;
+    let number2 = document.getElementById('num2').value;
+    document.write(parseInt(number1) + parseInt(number2));
+}
