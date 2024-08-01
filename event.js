93dd279100d38133420895291333ce34e4b2fde6
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-08-01 19:35:07 +0530

    add form validation

--- event.js
+++ event.js
@@ -92,12 +92,53 @@
 
 
 
-function fun1(){
-  let para =document.getElementById('show')
-  para.style.display = "block";
-}
+// function fun1(){
+//   let para =document.getElementById('show')
+//   para.style.display = "block";
+// }
 
-function fun2(){
-  let para=document.getElementById('show')
-  para.style.display = "none";
+// function fun2(){
+//   let para=document.getElementById('show')
+//   para.style.display = "none";
+// }
+// js-form validation   html- requred
+function savedata(){
+  // let s=document.getElementById('btn')
+  let name=document.getElementById('nm').value 
+  let phone=document.getElementById('phn').value 
+  let email=document.getElementById('em').value 
+  let pass=document.getElementById('pss').value 
+  let cpass=document.getElementById('cpss').value 
+  // document.write(name)
+  if(name==""){
+    alert("plese fill name !")
+    document.getElementById('nm').focus()
+    return false;
+    
+  }
+  else if(phone==""){
+    alert("plese fill phone no !")
+    document.getElementById('phn').focus()
+    return false;
+  }
+  else if(email==""){
+    alert("plese fill email !")
+    document.getElementById('em').focus()
+    return false;
+  }
+  else if(pass==""){
+    alert("plese fill pass !")
+    document.getElementById('pass').focus()
+    return false;
+  }
+  else if(cpass==""){
+    alert("plese fill cpass !")
+    document.getElementById('cpass').focus()
+    return false;
+  }
+
+
+
+
+
 }
