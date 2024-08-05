88bfb02ceeaf058f0971c7fb07823b19f29e69df
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-08-05 23:46:05 +0530

    addon map()

--- form-validation.js
+++ form-validation.js
@@ -1,29 +1,37 @@
-function savedata() {
-  let name = document.getElementById("nm").value;
-  let phone = document.getElementById("phn").value;
-  let email = document.getElementById("em").value;
-  let pass = document.getElementById("pss").value;
-  let cpass = document.getElementById("cpss").value;
-  // document.write(name)
-  if (name == "") {
-    alert("plese fill name !");
-    document.getElementById("nm").focus();
-    return false;
-  } else if (phone == "") {
-    alert("plese fill phone no !");
-    document.getElementById("phn").focus();
-    return false;
-  } else if (email == "") {
-    alert("plese fill email !");
-    document.getElementById("em").focus();
-    return false;
-  } else if (pass == "") {
-    alert("plese fill pass !");
-    document.getElementById("pass").focus();
-    return false;
-  } else if (cpass == "") {
-    alert("plese fill cpass !");
-    document.getElementById("cpass").focus();
-    return false;
-  }
+// function savedata() {
+//   let name = document.getElementById("nm").value;
+//   let phone = document.getElementById("phn").value;
+//   let email = document.getElementById("em").value;
+//   let pass = document.getElementById("pss").value;
+//   let cpass = document.getElementById("cpss").value;
+//   // document.write(name)
+//   if (name == "") {
+//     alert("plese fill name !");
+//     document.getElementById("nm").focus();
+//     return false;
+//   } else if (phone == "") {
+//     alert("plese fill phone no !");
+//     document.getElementById("phn").focus();
+//     return false;
+//   } else if (email == "") {
+//     alert("plese fill email !");
+//     document.getElementById("em").focus();
+//     return false;
+//   } else if (pass == "") {
+//     alert("plese fill pass !");
+//     document.getElementById("pass").focus();
+//     return false;
+//   } else if (cpass == "") {
+//     alert("plese fill cpass !");
+//     document.getElementById("cpass").focus();
+//     return false;
+//   }
+// }
+
+
+// Task
+let arr=[1,2,3,4,5]
+for(let i=0;i<=5;i++){
+document.write(arr[i]*arr[i])
+  
 }
---
+++ map.js
@@ -0,0 +1,8 @@
+let arr=[1,2,3,4,5]
+// for(let i=0;i<5;i++){
+// console.log(arr[i]*arr[i])
+  
+// }
+
+let p=arr.map((t)=>{return t*t})
+console.log(p)
