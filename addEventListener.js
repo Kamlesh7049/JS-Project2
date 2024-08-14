5676d37c6dbaafe07d6e6a9b6027a6088976c38b
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-08-14 19:30:16 +0530

    addon addEventListner

---
+++ addEventListener.js
@@ -0,0 +1,17 @@
+
+let mybtn=document.getElementById('btn')
+mybtn.addEventListener("click", function() {
+    document.getElementById('demo').innerHTML = "Hello";
+});
+
+mybtn.addEventListener("mouseenter",data)
+funtion data(){
+ document.getElementById("demo2").innerHTML=Math.random();
+
+};
+
+
+mybtn.removeEventListener("click",display);
+
+
+
