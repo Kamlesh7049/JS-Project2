85aef2e3dc6725a3cd80cde88047f971674741c7
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-08-26 20:14:20 +0530

    addon Math_mathod

--- addEventListener.js
+++ addEventListener.js
@@ -1,17 +1,54 @@
+// let mybtn = document.getElementById("btn");
+// mybtn.addEventListener("click", function () {
+//   document.getElementById("demo").innerHTML = "Hello";
+// });
 
-let mybtn=document.getElementById('btn')
-mybtn.addEventListener("click", function() {
-    document.getElementById('demo').innerHTML = "Hello";
-});
 
-mybtn.addEventListener("mouseenter",data)
-function data(){
- document.getElementById("demo2").innerHTML=Math.random();
+// document.getElementById("btn").addEventListener("mouseenter",data)
+//  function data(){ 
+// document.getElementById("demo2").innerHTML=Math.random();
 
-};
-
+//  };
 
 // mybtn.removeEventListener("click",display);
 
 
+
+
+
+// // document.getElementById("btn").addEventListener("mouseenter", data);
+
+// // function data() {
+// //     document.getElementById("demo2").innerHTML = Math.random();
+// // }
+
+
+
+
+
+// syntax 
+// addEventListener("event",function)
+
+// document.getElementById("click").addEventListener('click',
+//     function(){
+//         document.getElementById("demo").innerHTML="Click Right"
+//     }
+// )
+
+// function display(){
+//     document.getElementById("demo1").innerHTML="Right here"
+//     console.log("Succes");
+// }
+document.getElementById("click").addEventListener('mouseenter',display)
+let k=document.getElementById("click");
+k.addEventListener('mouseenter',dis)
+
+function dis(){
+    document.getElementById("demo2").innerHTML=Math.random();
+       
+};
+
+// // event remove
+
+k.removeEventListener("mouseenter",dis);
 
