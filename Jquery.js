ec2021ac9fe0d4b98149f0a6af6ea346a39072de
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-09-12 20:07:27 +0530

    hide show fadein fade out slide in slide out

--- Jquery.js
+++ Jquery.js
@@ -31,12 +31,43 @@ $(document).ready(function(){
     //  alert("Hover out")
     // }
 
-    $("input").focus(function(){
-        $(this).css("background-color","pink")
-    })
-    $("input").blur(function(){
-        $(this).css("background-color","red")
-    })
+    // $("input").focus(function(){
+    //     $(this).css("background-color","pink")
+    // })
+    // $("input").blur(function(){
+    //     $(this).css("background-color","red")
+    // })
 
-})
+
+    // $("#hide").click(function(){
+    //     $("p").hide(2000);
+    // });
+    // $("#show").click(function(){
+    //     $("p").show(2000);
+    // });
+    // $("btn").click(function(){
+    //     $("p").toggle()
+    // });
+
+    // $("#fadein").click(function(){
+    //     $(".box1").fadeIn()
+    //     $(".box2").fadeIn(1000)
+    //     $(".box3").fadeIn(3000)
+    // });
+    $("#fadeout").click(function(){
+        $(".box1").fadeOut()
+        $(".box2").fadeOut(1000)
+        $(".box3").fadeOut(3000)
+    });
+    $(".container").click(function(){
+        $(".content").slideDown();
+    });
+
+    $(".container").click(function(){
+        $(".content").slideUp();
+    });
+
+
+
+});
 
