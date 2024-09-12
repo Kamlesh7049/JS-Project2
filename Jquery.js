d0dcb49c97bf86a01eca87525748dd37dc0aeaa1
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-09-12 08:15:47 +0530

    something write

--- Jquery.js
+++ Jquery.js
@@ -1,6 +1,42 @@
 $(document).ready(function(){
-    $("Button").click(function(){
+    $("button").click(function(){
       $("p").hide()
     })
 
+    // $("button").dblclick(function(){
+    //     $("#test").hide()
+    // })
+
+    // $("button").click(function() {
+    //     $("p.new").hide()
+    // })
+    // $("#test").click(function(){
+    //     $(this).hide()
+    // })
+
+    // $("#mypara").mouseenter(function(){
+    //     alert("Mouse enter")
+    // })
+    // $("#mypara").mouseleave(function(){
+    //     alert("Mouse leave")
+    // })
+
+    // $("#mypara").hover(function(){
+    //     alert("Hover on")
+
+
+        
+    // }),
+    // function(){
+    //  alert("Hover out")
+    // }
+
+    $("input").focus(function(){
+        $(this).css("background-color","pink")
+    })
+    $("input").blur(function(){
+        $(this).css("background-color","red")
+    })
+
 })
+
